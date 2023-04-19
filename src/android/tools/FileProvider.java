df9fcdc6bf6271122f3f7298f14946dc066ec83f
安琦 <aq@sguo.info>
2023-04-19 13:30:00 +0800

    change androidx to android support-v4

--- src/android/tools/FileProvider.java
+++ src/android/tools/FileProvider.java
@@ -1,5 +1,5 @@
 package de.kolbasa.apkupdater.tools;
 
-public class FileProvider extends androidx.core.content.FileProvider {
+public class FileProvider extends android.support.v4.content.FileProvider {
     //
 }
